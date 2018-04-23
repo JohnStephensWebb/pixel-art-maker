f601a33434ba7a7d15e43f6edfa9295c7b8271af
John Stephens-Webb <JohnStephensWebb@Johns-MacBook-Pro.local>
2018-04-23 17:04:03 -0600

    Current color id

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Generating Pixel Count =======================================
   let canvas = document.getElementsByClassName("canvas")[0]
 
-  for (var i = 0; i < 2556; i++) {
+  for (var i = 0; i < 2553; i++) {
     let newDiv = document.createElement("div")
     newDiv.classList.add("pixel")
     canvas.appendChild(newDiv)
@@ -11,15 +11,18 @@ document.addEventListener("DOMContentLoaded", () => {
 // storing color on click ========================================
 
   let color;
+  let selectedColor = $(".selectedColor")
 
-  $(".btn").click(function() {
+  $(".btn").click(function(event) {
     color = $(event.target).css("background-color")
+    selectedColor.css("background-color", color)
   })
 
   function paintColor(event) {
     $(event.target).css("background-color", color)
   }
   $(".pixel").click(paintColor)
+
 
 
 
